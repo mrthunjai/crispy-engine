23d1783b50e71191a30e0778332bdaa0e4192c57
AmruthKWarrier <amruthkwarrier@gmail.com>
2026-09-25 23:23:26 +0530

    post merge fixes implemented

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...(process.env.STATIC_EXPORT === 'true' ? { output: 'export' } : {}),
-  trailingSlash: true,
+  ...(process.env.STATIC_EXPORT === 'true' ? { output: 'export', trailingSlash: true } : {}),
   images: { unoptimized: true },
   basePath: process.env.GITHUB_PAGES === 'true' ? '/crispy-engine' : ''
+
 }
 module.exports = nextConfig
 
